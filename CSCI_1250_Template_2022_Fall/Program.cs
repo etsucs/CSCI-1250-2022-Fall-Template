0a8ac58d6eeda3459789276049fbafcbaf6c2dd8
Jake Gillenwater <JakeGill70@gmail.com>
2022-09-20 18:30:52 -0400

    Initial commit

---
+++ CSCI_1250_Template_2022_Fall/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSCI_1250_Template_2022_Fall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
